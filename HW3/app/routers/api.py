d397dd180e27feb40e06709ce5a2ff7bec9a71c8
sunnyshoun <chej40505@gmail.com>
2025-03-22 02:24:24 +0800

    update api

--- HW3/app/routers/api.py
+++ HW3/app/routers/api.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Response, Query
 from app.backend.search import search_meta
 
-router = APIRouter(tags=["search"])  # 刪除 prefix="/api"
+router = APIRouter()
 
-@router.get("/search")
+@router.get("/search", tags=["search"])
 async def search(query: str = Query(..., min_length=1)):
     results = await search_meta(query)
     return {"results": results}
+
+@router.head("/uptime", tags=["uptime"])
+def uptime():
+    return Response(status_code=200)
